f8148a8d47d1e19f1b1ebb6deb67f332c2b3ae37
taixingbi <bitaihang@gmail.com>
2021-11-16 10:59:10 -0500

    add

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -5,14 +5,6 @@ public class App {
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("----------app start----------");
-        JmsProducer jmsProducer = new JmsProducer();
-        JmsConsumer jmsConsumer = new JmsConsumer();
-
-        jmsProducer.MessageSender();
-
-        Thread.sleep(1000);
-
-        jmsConsumer.MessageReceiver();
 
         System.out.println("----------app end----------");
     }
